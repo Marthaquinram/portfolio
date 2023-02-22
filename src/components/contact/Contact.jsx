495d47dd1f53444af5e9350ab1b30da0a55ca220
Martha Quintanilla-Ramirez <98363191+Marthaquinram@users.noreply.github.com>
2023-02-21 23:56:21 -0800

    Revert "Dev"

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,65 +1,8 @@
-import React, { useRef } from "react";
+import React from "react";
 import "./contact.css";
-import { HiOutlineMail } from "react-icons/hi";
-import emailjs from "emailjs-com";
 
 const Contact = () => {
-  const form = useRef();
-  const sendEmail = (e) => {
-    e.preventDefault();
-    emailjs.sendForm(
-      "service_l82r1ps",
-      "template_u7stuyr",
-      form.current,
-      "Hg7EkAh_5XMo7KIli"
-    );
-    e.target.reset();
-  };
-  return (
-    <section id="contact">
-      <h5>Get In Touch</h5>
-      <h2>Contact Me</h2>
-      <div className="container contact__container">
-        <div className="contact__options">
-          <article className="contact__option">
-            <HiOutlineMail className="contact__option-icon" />
-            <h4>Email</h4>
-            <h5>marthaquin21@gmail.com</h5>
-            <a href="mailto:marthaquin21@.com" target="_blank" rel="noreferrer">
-              Send a message
-            </a>
-          </article>
-          <article className="contact__option">
-            <HiOutlineMail className="contact__option-icon" />
-            <h4>Email</h4>
-            <h5>marthaquin21@gmail.com</h5>
-            <a href="mailto:marthaquin21@.com" target="_blank" rel="noreferrer">
-              Send a message
-            </a>
-          </article>
-        </div>
-        {/* END OF CONTACT OPTIONS */}
-        <form ref={form} onSubmit={sendEmail}>
-          <input
-            type="text"
-            name="name"
-            placeholder="Your Full Name"
-            required
-          />
-          <input type="email" name="email" placeholder="Your Email" required />
-          <textarea
-            name="message"
-            rows="7"
-            placeholder="Your Message"
-            required
-          ></textarea>
-          <button type="submit" className="btn btn-primary">
-            Send Message
-          </button>
-        </form>
-      </div>
-    </section>
-  );
+  return <section id="contact">Contact</section>;
 };
 
 export default Contact;
