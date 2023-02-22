27250700b3e8367082bfdd143ee51abbcb9a3abb
Martha Quintanilla-Ramirez <98363191+Marthaquinram@users.noreply.github.com>
2023-02-22 13:57:25 -0800

    contact form
    * starting contact component
    
    * completed contact component and contact css
    
    * started working on the footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,50 @@
 import React from "react";
 import "./footer.css";
+import { FaGithub } from "react-icons/fa";
+import { BsLinkedin } from "react-icons/bs";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <a href="#" className="footer__logo">
+        Martha
+      </a>
+      <ul className="premalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+      <div className="footer__socials">
+        <a href="https://github.com/Marthaquinram" target="_blank">
+          <FaGithub />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/martha-quintanilla-ramirez/"
+          target="_blank"
+        >
+          <BsLinkedin />
+        </a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
