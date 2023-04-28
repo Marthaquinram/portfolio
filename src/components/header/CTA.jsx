ca4f354421d854ba979650d629e158747573d658
Martha Quintanilla-Ramirez <98363191+Marthaquinram@users.noreply.github.com>
2023-04-27 22:01:56 -0700

    Dev (#28)
    * completed footer component and footer css
    
    * updated projects and working on fine tuning links and CSS
    
    * updated readme
    
    * made some change to css and updated links to sites
    
    * updated some of the project and testimonials and color scheme
    
    * changed some colors
    
    * removed random div
    
    * updated the resume that was downloadable, and played around with color scheme

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import CV from "../../assets/cv.pdf";
+import CV from "../../assets/resume.pdf";
 
 const CTA = () => {
   return (
     <div className="cta">
       <a href={CV} download className="btn">
-        Download CV
+        Download Resume
       </a>
       <a href="#contact" className="btn btn-primary">
         Let's Connect
